cf7c06f3ccd8796b5a752a6df497dd677636331b
James Curry <BobChubby@users.noreply.github.com>
2014-12-27 00:26:50 -0500

    Removed unnecessary code
    Because the sleeper function is defined within the sleepSort function, the scope, length, and callback could be accessed from within

--- sleepSort.js
+++ sleepSort.js
@@ -3,18 +3,18 @@
 // Original idea taken from http://dis.4chan.org/read/prog/1295544154
 
 function sleepSort(items,callback) {
-	function sleeper(item,length,scope,callback) {
+	var scope = this;
+	scope.sorted = [];
+	function sleeper(item) {
 		return function() {
 			scope.sorted.push(item);
-			if(scope.sorted.length == length) {
+			if(scope.sorted.length == items.length) {
 				callback(scope.sorted);
 			}
 		}
 	}
-	var self = this;
-	self.sorted = [];
 	for(var i=0;i<items.length;i++) {
 		var item = items[i];
-		setTimeout(sleeper(item,items.length,self,callback),item);
+		setTimeout(sleeper(item),item);
 	}
 }
